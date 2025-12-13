a47d7137d887557dc60beea5c5179695cdcc8db6
Melvin Chen <melvin.chen@makenotion.com>
2025-12-13 03:17:02 -0500

    refactor divider styles

--- src/popup/App.svelte
+++ src/popup/App.svelte
@@ -276,8 +276,11 @@
 
       <div class="w-full h-px relative overflow-hidden">
         <div
-          class="absolute inset-0 h-full w-full bg-gradient-to-r from-slate-200 via-slate-200 to-white to-100% via-90%"
-        ></div>
+          class={cn(
+            'absolute inset-0 h-full w-full',
+            'bg-gradient-to-r from-slate-200 via-slate-200 to-white to-100% via-90%',
+          )}
+        />
       </div>
 
       <section>
