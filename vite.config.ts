bd1a6a354f4e47adce6bf236bc07ac55d55ff5d1
Melvin Chen <melvin.chen@makenotion.com>
2025-12-13 13:17:44 -0500

    use actual project root as root directory so we don't need to keep specifying it in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,45 +3,26 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { crx } from '@crxjs/vite-plugin'
 import checker from 'vite-plugin-checker'
 import zipPack from 'vite-plugin-zip-pack'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 import svelteConfig from './svelte.config'
 import manifest from './src/manifest.json'
 
-const currentDir = fileURLToPath(new URL('.', import.meta.url))
-const rootDir = path.resolve(currentDir, 'src')
-
 export default defineConfig({
-  root: rootDir,
-  publicDir: path.resolve(currentDir, 'public'),
   plugins: [
-    svelte({
-      ...svelteConfig,
-      inspector: true,
-    }),
+    svelte({ ...svelteConfig, inspector: true }),
     crx({ manifest }),
-    checker({
-      typescript: true,
-    }),
-    zipPack({
-      outDir: currentDir,
-      outFileName: 'extension.zip',
-    }),
+    checker({ typescript: true }),
+    zipPack({ outFileName: 'extension.zip' }),
   ],
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
     },
   },
-  build: {
-    outDir: path.resolve(currentDir, 'dist'),
-    emptyOutDir: true,
-  },
   resolve: {
     alias: {
-      $shared: path.resolve(rootDir, 'shared'),
+      $shared: '/src/shared',
     },
   },
 })
